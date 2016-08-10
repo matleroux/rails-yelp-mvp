9ae12064a2309cf5e9582de52514b260ab861232
Mathieu Le Roux <mathieulr@gmail.com>
2016-08-10 15:48:53 -0300

    back end done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: %w(chinese italian french japanese belgian), allow_nill: false }, presence: true
+  has_many :reviews, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "+33145674637",
+    category:     "french"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number: "+33145874637",
+    category:     "french"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number: "+33183674637",
+    category:     "french"
+  },
+  {
+    name:         "Hoa Binh",
+    address:      "Avenue Pierre Loti 83200 La Garde",
+    phone_number: "+33456789009",
+    category:     "chinese"
+  },
+  {
+    name:         "Pepe de Napoli",
+    address:      "Avenue Charles de Gaulle 59000 Lille",
+    phone_number: "+33246379300",
+    category:     "italian"
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
